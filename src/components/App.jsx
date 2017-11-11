3a8ef8c8cd749804b2ac45ab6ac73a11e9c7a71e
Timon <timonzimm@gmail.com>
2017-11-11 20:34:02 +0100

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
+import Button from 'material-ui/Button';
+import axios from 'axios';
 
 const styles = theme => ({
   root: {
@@ -16,40 +18,43 @@ const styles = theme => ({
   },
 });
 
-function App(props) {
-  const { classes } = props;
 
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={24}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    axios.get('https://api.ciscospark.com/v1/rooms',
+      {
+        "headers": {
+          "Content-type": "application/json; charset=utf-8",
+          "Authorization": "Bearer ZTNjOTNhZmQtOGIxYi00MDI1LTlhZjktM2RjMDdmNmU3YWRlMmNmOGYxMDAtYmQy"
+        }
+      }).then(e => {
+        this.setState({ patients: e.data.items.filter(room => room.title.startsWith("Patient")) });
+      })
+  }
+  render() {
+    return (
+      <div className={this.props.classes.root}>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Paper className={this.props.classes.paper}>Menu</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={this.props.classes.paper}>Patient (rooms)</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={this.props.classes.paper}>6</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={this.props.classes.paper}><p>Vertical timeline</p></Paper>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-      </Grid>
-    </div>
-  );
+      </div>
+    );
+  }
 }
-
-App.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(App)
